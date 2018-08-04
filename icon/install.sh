284def32172f075d29fef37d37ba4ed931de80db
graypawn <choi.pawn@gmail.com>
2018-08-04 19:11:47 +0900

    일부 ICON을 png->svg

--- icon/install.sh
+++ icon/install.sh
@@ -2,15 +2,22 @@
 # -*- coding: utf-8 -*-
 # Author: graypawn (choi.pawn @gmail.com)
 # Created: 2018-02-06
-
 BASEDIR=$(dirname "$0")
+LOCAL_ICON_DIR=$HOME/.local/share/icons/hicolor
 
 install_icon_with_size () {
+    local size=$1
     local icon_size_dir=$BASEDIR/$1
     for item in `ls -A $icon_size_dir`; do
-        xdg-icon-resource install --size $1 $icon_size_dir/$item
+        if [ $size = "scalable" ]; then
+            cp $icon_size_dir/$item $LOCAL_ICON_DIR/scalable/apps/
+        else
+            xdg-icon-resource install --size $size $icon_size_dir/$item
+        fi
     done
 }
 
 install_icon_with_size 48
 install_icon_with_size 128
+install_icon_with_size "scalable"
+
