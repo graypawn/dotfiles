6b10e48f4be0e78285667ed37271759345427a92
graypawn <choi.pawn@gmail.com>
2018-08-04 16:02:03 +0900

    Emacs 부분통합

--- pkg-script/emacs.sh
+++ pkg-script/emacs.sh
@@ -2,12 +2,11 @@
 OPTIONS=${@:3}
 EMACS_DIR=$HOME/.emacs.d
 sudo pacman $@
+sudo pacman -S adobe-source-code-pro-fonts $OPTIONS
 git clone git@github.com:graypawn/emacs.git $EMACS_DIR
-cd $EMACS_DIR
-git submodule init
-git submodule update
-sudo cp $EMACS_DIR/utils/emacs-daemon /usr/local/bin/emacs-daemon
+cd $EMACS_DI
+
+# Daemon
 mkdir -p ~/.config/systemd/user/
 cp $EMACS_DIR/utils/emacs.service ~/.config/systemd/user/
-sudo pacman -S aspell-en $OPTIONS
 systemctl --user enable emacs
