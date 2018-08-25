a651388d744d09c6a01669e69a86bc2e946d4384
graypawn <choi.pawn@gmail.com>
2018-08-25 23:29:03 +0900

    emacs -> spacemacs

--- pkg-script/spacemacs.sh
+++ pkg-script/spacemacs.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 OPTIONS=${@:3}
+BASEDIR=$(dirname "$0")
 EMACS_DIR=$HOME/.emacs.d
+DATA_DIR=$BASEDIR/../data
 sudo pacman $@
 sudo pacman -S ttf-baekmuk $OPTIONS
 sudo pacman -S adobe-source-code-pro-fonts $OPTIONS
 sex emacs-mozc
-git clone git@github.com:graypawn/emacs.git $EMACS_DIR
-cd $EMACS_DIR
+git clone git@github.com:graypawn/spacemacs.git $EMACS_DIR
 
 # Daemon
 mkdir -p ~/.config/systemd/user/
-cp $EMACS_DIR/utils/emacs.service ~/.config/systemd/user/
+cp $DATA_DIR/emacs.service $HOME/.config/systemd/user/
 systemctl --user enable emacs
